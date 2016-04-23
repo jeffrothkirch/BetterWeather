6d60dacaa2df90ef278fcc3ac882aa59d0ae632d
theleastinterestingcoder <quanzhou64@gmail.com>
2016-04-23 12:47:03 -0400

    tutorial for setting up a restful api

--- WeatherService/weather_service.py
+++ WeatherService/weather_service.py
@@ -1,9 +1,34 @@
-from flask import Flask
+#!flask/bin/python
+from flask import Flask, jsonify
+
 app = Flask(__name__)
 
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web', 
+        'done': False
+    }
+]
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': task[0]})
+
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def examples():
+    return "Example:\ncurl -i http://localhost:8081/todo/api/v1.0/tasks/1\n"
+
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=8081)
+    app.run(host='localhost', port=8081, debug=True)
