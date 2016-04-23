e9f608b66f9fe98bdaa5c309a2a2212ea987487a
theleastinterestingcoder <quanzhou64@gmail.com>
2016-04-23 16:00:52 -0400

    mocked out something that steve can use

--- WeatherService/weather_service.py
+++ WeatherService/weather_service.py
@@ -1,34 +1,22 @@
 #!flask/bin/python
-from flask import Flask, jsonify, abort
+from flask import Flask, jsonify, abort, json
 
 app = Flask(__name__)
 
-data = [
-    {
-        'zip_code': '11729',
-        'date' : '2008-10-30',
-        'temperature_F': 84.3,
-        'description': u'sunny', 
-    },
-    {
-        'zip_code': '08544',
-        'date' : '2008-10-30',
-        'temperature_F': 84.3,
-        'description': u'sunny', 
-    }
-]
 
-@app.route('/<zip_code>/<date>', methods=['GET'])
-def get_task(zip_code):
-    matching_data = [d for d in data if data['zip_code'] == zip_code]
-    if len(matching_data) == 0:
-        abort(404)
-    return jsonify({'task': task[0]})
+filename = '04_2016_dump.json'
+file_information = (open(filename, 'r')).read()
+data = json.loads(file_information)
+
+
+@app.route('/cached/<location>/<start_time>/<end_time>', methods=['GET'])
+def get_task(location, start_time, end_time):
+    return jsonify(result=data)
 
 @app.route('/')
 def examples():
-    return "Example:\ncurl -i http://localhost:8081/todo/api/v1.0/tasks/1\n"
+    return "Example:\ncurl -i http://localhost:8081/cached/new_york_city/2016-04-23/2015-04-23\n"
 
-def invalid_argument()
+
 if __name__ == '__main__':
     app.run(host='localhost', port=8081, debug=True)
