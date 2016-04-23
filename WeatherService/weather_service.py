59b6b6d8b80d5b6ebe8d7ab506b4a7887e282a6b
theleastinterestingcoder <quanzhou64@gmail.com>
2016-04-23 15:42:35 -0400

    got the data dump transformation from weather underground

--- WeatherService/weather_service.py
+++ WeatherService/weather_service.py
@@ -1,27 +1,27 @@
 #!flask/bin/python
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort
 
 app = Flask(__name__)
 
-tasks = [
+data = [
     {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
-        'done': False
+        'zip_code': '11729',
+        'date' : '2008-10-30',
+        'temperature_F': 84.3,
+        'description': u'sunny', 
     },
     {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web', 
-        'done': False
+        'zip_code': '08544',
+        'date' : '2008-10-30',
+        'temperature_F': 84.3,
+        'description': u'sunny', 
     }
 ]
 
-@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
-def get_task(task_id):
-    task = [task for task in tasks if task['id'] == task_id]
-    if len(task) == 0:
+@app.route('/<zip_code>/<date>', methods=['GET'])
+def get_task(zip_code):
+    matching_data = [d for d in data if data['zip_code'] == zip_code]
+    if len(matching_data) == 0:
         abort(404)
     return jsonify({'task': task[0]})
 
@@ -29,6 +29,6 @@ def get_task(task_id):
 def examples():
     return "Example:\ncurl -i http://localhost:8081/todo/api/v1.0/tasks/1\n"
 
-
+def invalid_argument()
 if __name__ == '__main__':
     app.run(host='localhost', port=8081, debug=True)
