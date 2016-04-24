f67a4a98284386fbe61771d7d61c14c9a2d2ee36
Jeff Rothkirch <jeff.rothkirch@gmail.com>
2016-04-24 07:26:31 +0000

    made image service more stable when watson doesn't reply back in time

--- services/imageclassifier/image_service.py
+++ services/imageclassifier/image_service.py
@@ -102,10 +102,12 @@ def filter():
 
 def is_outdoors(data_entry):
     # names = ['Outdoors', 'Burning', 'Racquet_Sport', 'Bat_Sport', 'Team_Field_Sport', 'Team_Indoor_Sport', 'Team_Sport', 'Track_and_Field', 'Boating', 'Power_Boating', 'Rowing', 'Swimming', 'Water_Sport', 'Ice_Sport', 'Skiing', 'Snow_Sport', 'Winter_Sport', 'Sports_Field', 'Sports_Track', 'Urban_Scene', 'Nature_Scene', 'Water_Scene', 'Winter_Scene', 'Outdoors', 'Sky_Scene', 'Flower', 'Wild_Fire', 'Earthquake', 'Flood', 'Storm', 'Camping', 'Wedding', 'Concert', 'Adventure_Sport', 'Climbing', 'Land_Sailing', 'Air_Sport', 'Ballooning', 'Hand_Gliding', 'Camel_Racing', 'Dog_Racing', 'Equestrian', 'Horce_Racing', 'Polo', Golf', ;Greco_Roman_Wrestling', 'Mud_Wrestling', 'Cycling', 'Fishing', 'Rollerskating', 'Skateboarding', 'American_Football', 'Football', 'Soccer', 'Rugby', 'Field_Hocky', 'Track', 'Jet_Skiing', 'Crew', 'Sailing', 'Windsurfing', 'Cliff_Diving', 'Sledding', 'Tobagganing', 'Ski_Jumping']
-    scores = [(d['classifier_id'], d['score']) for d in data_entry['scores']]
-    filtered_scores = [s  for s in scores if s[0].lower() in nature_set and s[1] > 0.6]
+    if ('scores' in data_entry):
+        scores = [(d['classifier_id'], d['score']) for d in data_entry['scores']]
+        filtered_scores = [s  for s in scores if s[0].lower() in nature_set and s[1] > 0.6]
 
-    return len(filtered_scores) > 0
+        return len(filtered_scores) > 0
+    return False
     
 
 def delete_folder(path):
