b84bff1cff0673e0f2f926d2f997adb7797445a6
theleastinterestingcoder <quanzhou64@gmail.com>
2016-04-24 12:32:57 -0400

    finishing commit for all weather data

--- services/weather/weather_service.py
+++ services/weather/weather_service.py
@@ -4,14 +4,22 @@ from flask import Flask, jsonify, abort, json
 app = Flask(__name__)
 
 
-filename = 'data.json'
-file_information = (open(filename, 'r')).read().replace( '\x00', '' )
-data = json.loads(file_information)
+filename5 = 'data.json'
+filename12 = 'all_data.json'
+
+file_information5 = (open(filename5, 'r')).read().replace( '\x00', '' )
+year5_data = json.loads(file_information5)
+file_information12 = (open(filename12, 'r')).read().replace( '\x00', '' )
+year12_data = json.loads(file_information12)
 
 
 @app.route('/cached/<location>/<start_time>/<end_time>', methods=['GET'])
 def get_task(location, start_time, end_time):
-    return jsonify(result=data)
+    return jsonify(result=year5_data)
+
+@app.route('/all/<location>/<start_time>/<end_time>', methods=['GET'])
+def get_all(location, start_time, end_time):
+    return jsonify(result=year12_data)
 
 @app.route('/')
 def examples():
